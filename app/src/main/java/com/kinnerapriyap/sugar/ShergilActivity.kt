aa0f335ffe8be95e75c1bdc90070206d36322cf7
kinnerapriyap <kinnerapriyap@gmail.com>
2020-05-18 18:19:35 +0900

    Add library to sample with new ShergilActivity

---
+++ app/src/main/java/com/kinnerapriyap/sugar/ShergilActivity.kt
@@ -0,0 +1,12 @@
+package com.kinnerapriyap.sugar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShergilActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shergil)
+    }
+}
--- sample/src/main/java/com/kinnerapriyap/sample/MainActivity.kt
+++ sample/src/main/java/com/kinnerapriyap/sample/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.kinnerapriyap.sample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.kinnerapriyap.sugar.ShergilActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.hello).setOnClickListener {
+            startActivity(
+                Intent(this, ShergilActivity::class.java)
+            )
+        }
     }
 }
