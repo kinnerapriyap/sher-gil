2d40d4a4a92f52698dd3a470fec1370ebcc85429
kinnerapriyap <kinnerapriyap@gmail.com>
2020-05-23 21:38:56 +0900

    Ask READ_EXTERNAL_STORAGE permission

--- sher-gil/src/main/java/com/kinnerapriyap/sugar/ShergilActivity.kt
+++ sher-gil/src/main/java/com/kinnerapriyap/sugar/ShergilActivity.kt
@@ -1,9 +1,13 @@
 package com.kinnerapriyap.sugar
 
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.kinnerapriyap.sugar.GetMultipleFromGallery.Companion.RESULT_URIS
 import java.util.ArrayList
@@ -22,6 +26,12 @@ class ShergilActivity : AppCompatActivity() {
             setResultAndFinish(resultIntent)
         }
 
+    private val askReadStoragePermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { allowed ->
+            if (allowed) openGallery()
+            else setCancelledAndFinish()
+        }
+
     companion object {
         fun createIntent() = ShergilActivity()
     }
@@ -30,12 +40,14 @@ class ShergilActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shergil)
 
-        getMultipleFromGallery.launch(
-            GetMultipleFromGalleryInput(
-                isOnlyFromLocal = false,
-                isMultipleAllowed = true
-            )
-        )
+        when {
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(
+                READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED ->
+                askReadStoragePermission.launch(READ_EXTERNAL_STORAGE)
+            else ->
+                openGallery()
+        }
     }
 
     override fun onDestroy() {
@@ -43,8 +55,27 @@ class ShergilActivity : AppCompatActivity() {
         getMultipleFromGallery.unregister()
     }
 
+    override fun onBackPressed() {
+        setResult(Activity.RESULT_CANCELED)
+        super.onBackPressed()
+    }
+
     private fun setResultAndFinish(result: Intent) {
         setResult(Activity.RESULT_OK, result)
         finish()
+    }
+
+    private fun setCancelledAndFinish() {
+        setResult(Activity.RESULT_CANCELED)
+        finish()
+    }
+
+    private fun openGallery() {
+        getMultipleFromGallery.launch(
+            GetMultipleFromGalleryInput(
+                isOnlyFromLocal = false,
+                isMultipleAllowed = true
+            )
+        )
     }
 }
