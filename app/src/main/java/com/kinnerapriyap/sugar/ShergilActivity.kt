98b7330f98b23beec7aaaa6711e42a1e0f441985
kinnerapriyap <kinnerapriyap@gmail.com>
2020-05-23 15:47:45 +0900

    Update gradle version

--- app/src/main/java/com/kinnerapriyap/sugar/ShergilActivity.kt
+++ app/src/main/java/com/kinnerapriyap/sugar/ShergilActivity.kt
@@ -2,8 +2,12 @@ package com.kinnerapriyap.sugar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class ShergilActivity : AppCompatActivity() {
+
+    private val container: View
+        get() = findViewById(R.id.container)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
