1b0a192af5f36cc0a2d7dd7377cbf553abd1d62e
kinnerapriyap <kinnerapriyap@gmail.com>
2020-05-23 18:56:50 +0900

    Remove unused import

--- sher-gil/src/main/java/com/kinnerapriyap/sugar/ShergilActivity.kt
+++ sher-gil/src/main/java/com/kinnerapriyap/sugar/ShergilActivity.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import java.util.ArrayList
