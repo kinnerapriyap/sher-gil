bf7efd386d49b0885b0eec13117aaac20a2ea7ac
kinnerapriyap <kinnerapriyap@gmail.com>
2020-05-24 19:45:18 +0900

    Implement new entry Shergil in sample MainActivity

--- sample/src/main/java/com/kinnerapriyap/sample/MainActivity.kt
+++ sample/src/main/java/com/kinnerapriyap/sample/MainActivity.kt
@@ -1,21 +1,37 @@
 package com.kinnerapriyap.sample
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
-import com.kinnerapriyap.sugar.ShergilActivity
+import com.kinnerapriyap.sugar.MimeType
+import com.kinnerapriyap.sugar.Shergil
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val REQUEST_SHERGIL = 1234
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         findViewById<TextView>(R.id.hello).setOnClickListener {
-            startActivity(
-                Intent(this, ShergilActivity::class.java)
-            )
+            Shergil.create(this)
+                .mimeTypes(MimeType.IMAGES)
+                .allowOnlyLocalStorage(false)
+                .allowMultiple(true)
+                .withRequestCode(REQUEST_SHERGIL)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_SHERGIL) {
+            findViewById<TextView>(R.id.hello).text =
+                Shergil.getMediaUris(data).toString().replace(", ", "\n")
         }
     }
 }
