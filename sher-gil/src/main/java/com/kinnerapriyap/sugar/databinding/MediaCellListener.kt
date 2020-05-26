7e97c86fc0e65d96362565477dc6985aa6166c6a
kinnerapriyap <kinnerapriyap@gmail.com>
2020-05-26 23:30:29 +0900

    Update MediaCellListener

--- sher-gil/src/main/java/com/kinnerapriyap/sugar/databinding/MediaCellListener.kt
+++ sher-gil/src/main/java/com/kinnerapriyap/sugar/databinding/MediaCellListener.kt
@@ -1,8 +1,7 @@
 package com.kinnerapriyap.sugar.databinding
 
 import android.net.Uri
-import android.view.View
 
 interface MediaCellListener {
-    fun onMediaCellClicked(view: View, uri: Uri)
+    fun onMediaCellClicked(uri: Uri)
 }
