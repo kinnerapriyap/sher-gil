8127acb33ad7133bcb8366a05d0db4948e9aab26
kinnerapriyap <kinnerapriyap@gmail.com>
2020-05-26 20:06:28 +0900

    Add cell to ShergilController

--- sher-gil/src/main/java/com/kinnerapriyap/sugar/ShergilController.kt
+++ sher-gil/src/main/java/com/kinnerapriyap/sugar/ShergilController.kt
@@ -1,9 +1,23 @@
 package com.kinnerapriyap.sugar
 
+import android.net.Uri
 import com.airbnb.epoxy.EpoxyController
 
 class ShergilController : EpoxyController() {
-    override fun buildModels() {
 
+    private var mediaList: List<Uri> = emptyList()
+
+    fun setMediaList(mediaList: List<Uri>) {
+        this.mediaList = mediaList
+        requestModelBuild()
+    }
+
+    override fun buildModels() {
+        mediaList.forEach { uri ->
+            cell {
+                id(uri.path)
+                mediaUri(uri)
+            }
+        }
     }
 }
