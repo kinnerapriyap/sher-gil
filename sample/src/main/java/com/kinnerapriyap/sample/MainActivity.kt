94af24447f844f14f7c54ac40e87b29d68c18bd0
kinnerapriyap <kinnerapriyap@gmail.com>
2020-05-26 12:21:38 +0900

    Add title to sample MainActivity to differentiate

--- sample/src/main/java/com/kinnerapriyap/sample/MainActivity.kt
+++ sample/src/main/java/com/kinnerapriyap/sample/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        title = "sample"
 
         findViewById<TextView>(R.id.hello).setOnClickListener {
             Shergil.create(this)
