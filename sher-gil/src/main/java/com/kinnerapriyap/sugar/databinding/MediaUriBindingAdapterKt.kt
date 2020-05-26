d2eba928c7cc0480992d5758231f7ae6c753847c
kinnerapriyap <kinnerapriyap@gmail.com>
2020-05-26 19:46:58 +0900

    Add bindMediaUri binding adapter for loading image

---
+++ sher-gil/src/main/java/com/kinnerapriyap/sugar/databinding/MediaUriBindingAdapterKt.kt
@@ -0,0 +1,22 @@
+package com.kinnerapriyap.sugar.databinding
+
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+
+@BindingAdapter("mediaUri")
+fun bindMediaUri(imageView: ImageView, uri: Uri) {
+    val bitmap =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val source =
+                ImageDecoder.createSource(imageView.context.contentResolver, uri)
+            ImageDecoder.decodeBitmap(source)
+        } else {
+            MediaStore.Images.Media.getBitmap(imageView.context.contentResolver, uri)
+        }
+    imageView.setImageBitmap(bitmap)
+    //imageView.setImageURI(uri)
+}
