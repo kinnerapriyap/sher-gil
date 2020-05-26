a5c6951e817fd2a4c7e92d93e1661f61dd36176d
kinnerapriyap <kinnerapriyap@gmail.com>
2020-05-26 19:44:05 +0900

    Add package-info.java for data binding pattern

---
+++ sher-gil/src/main/java/com/kinnerapriyap/sugar/databinding/package-info.java
@@ -0,0 +1,5 @@
+@EpoxyDataBindingPattern(rClass = R.class, layoutPrefix = "view_media")
+package com.kinnerapriyap.sugar.databinding;
+
+import com.airbnb.epoxy.EpoxyDataBindingPattern;
+import com.kinnerapriyap.sugar.R;
