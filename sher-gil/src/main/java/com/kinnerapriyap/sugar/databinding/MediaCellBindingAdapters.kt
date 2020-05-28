a7767fdf30fd0f17f73b29959839e52756eaaed8
kinnerapriyap <kinnerapriyap@gmail.com>
2020-05-28 13:53:55 +0900

    Rename to MediaCellBindingAdapters and use Glide to load

---
+++ sher-gil/src/main/java/com/kinnerapriyap/sugar/databinding/MediaCellBindingAdapters.kt
@@ -0,0 +1,19 @@
+package com.kinnerapriyap.sugar.databinding
+
+import android.net.Uri
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.google.android.material.card.MaterialCardView
+
+@BindingAdapter("mediaUri")
+fun ImageView.bindMediaUri(uri: Uri) {
+    Glide.with(context)
+        .load(uri)
+        .into(this@bindMediaUri)
+}
+
+@BindingAdapter("android:checked_state")
+fun MaterialCardView.bindCheckedState(isChecked: Boolean) {
+    this.isChecked = isChecked
+}
