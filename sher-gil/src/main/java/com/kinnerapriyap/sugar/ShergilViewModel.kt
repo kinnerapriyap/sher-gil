e98a359bd4fe724bc0f8a5d6f0f467cf27d884bc
kinnerapriyap <kinnerapriyap@gmail.com>
2020-06-06 00:17:29 +0900

    Pass mimeTypes to fetchMedia and query with selection

--- sher-gil/src/main/java/com/kinnerapriyap/sugar/ShergilViewModel.kt
+++ sher-gil/src/main/java/com/kinnerapriyap/sugar/ShergilViewModel.kt
@@ -28,7 +28,7 @@ class ShergilViewModel(application: Application) : AndroidViewModel(application)
     private val updatedMediaCellPosition = MutableLiveData<MediaCellUpdateModel>()
 
     private var cursor: LiveData<Cursor?> = liveData {
-        emit(mediaGalleryHandler.fetchMedia())
+        emit(mediaGalleryHandler.fetchMedia(choiceSpec.mimeTypes))
     }
 
     /**
