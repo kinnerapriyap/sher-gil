8cce702035704f4427b3f2304c561503bf838a12
kinnerapriyap <kinnerapriyap@gmail.com>
2020-06-07 16:20:39 +0900

    Remove unused import

--- sher-gil/src/main/java/com/kinnerapriyap/sugar/ShergilActivity.kt
+++ sher-gil/src/main/java/com/kinnerapriyap/sugar/ShergilActivity.kt
@@ -13,7 +13,6 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import com.kinnerapriyap.sugar.choice.ChoiceSpec
 import com.kinnerapriyap.sugar.mediagallery.album.MediaGalleryAlbumCursorAdapter
 import com.kinnerapriyap.sugar.mediagallery.MediaGalleryFragment
 import com.kinnerapriyap.sugar.mediagallery.MediaGalleryFragmentListener
