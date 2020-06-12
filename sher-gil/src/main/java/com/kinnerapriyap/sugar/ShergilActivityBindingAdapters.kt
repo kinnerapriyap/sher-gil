51035e963686b5742f825178ba10b37c0856ca3c
kinnerapriyap <kinnerapriyap@gmail.com>
2020-06-12 19:27:09 +0900

    Set color state drawables for apply and preview buttons

--- sher-gil/src/main/java/com/kinnerapriyap/sugar/ShergilActivityBindingAdapters.kt
+++ sher-gil/src/main/java/com/kinnerapriyap/sugar/ShergilActivityBindingAdapters.kt
@@ -5,7 +5,6 @@ import androidx.databinding.BindingAdapter
 
 @BindingAdapter("selectedCount")
 fun TextView.bindSelectedCount(selectedCount: Int) {
-    isEnabled = selectedCount != 0
     text = StringBuilder().apply {
         append(resources.getString(R.string.apply))
         if (selectedCount != 0) {
