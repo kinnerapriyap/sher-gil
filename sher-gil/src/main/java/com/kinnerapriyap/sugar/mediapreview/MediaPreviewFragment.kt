d0d8f20326908b3ba9c62faa09ca80b47dc36544
kinnerapriyap <kinnerapriyap@gmail.com>
2020-06-12 18:00:54 +0900

    Add MediaPreviewFragment with viewpager2 and tablayout

---
+++ sher-gil/src/main/java/com/kinnerapriyap/sugar/mediapreview/MediaPreviewFragment.kt
@@ -0,0 +1,61 @@
+package com.kinnerapriyap.sugar.mediapreview
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import com.google.android.material.tabs.TabLayoutMediator
+import com.kinnerapriyap.sugar.R
+import com.kinnerapriyap.sugar.ShergilViewModel
+import kotlinx.android.synthetic.main.fragment_media_preview.*
+
+class MediaPreviewFragment : Fragment() {
+
+    private val viewModel: ShergilViewModel by activityViewModels()
+
+    private val mediaPreviewAdapter by lazy {
+        MediaPreviewAdapter(this)
+    }
+
+    private var listener: MediaPreviewFragmentListener? = null
+
+    companion object {
+        fun newInstance() =
+            MediaPreviewFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_media_preview, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        listener?.hideToolbars()
+
+        viewPager.adapter = mediaPreviewAdapter
+
+        TabLayoutMediator(tabDots, viewPager) { tab, position ->
+        }.attach()
+
+        viewModel.getSelectedMedia().observe(
+            requireActivity(),
+            Observer {
+                mediaPreviewAdapter.selectedMedia = it
+            }
+        )
+    }
+
+    override fun onDestroyView() {
+        listener?.showToolbars()
+        super.onDestroyView()
+    }
+
+    fun setMediaPreviewFragmentListener(listener: MediaPreviewFragmentListener) {
+        this.listener = listener
+    }
+}
