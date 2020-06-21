51789cd032b606eb337312fcd3d3983c950932d1
Kinnera Priya <kinnerapriyap@gmail.com>
2020-06-21 10:19:07 +0900

    Media placeholder (#45)
    * Remove src from media ImageView
    
    * Add error and placeholder attrs
    
    * Add values in style for error and placeholder attrs
    
    * Apply error and placeholder in mediaUri
    
    * Apply error and placeholder in mediaUri
    
    * Add TODO to replace default drawables
    
    * Remove unused test files

--- sher-gil/src/main/java/com/kinnerapriyap/sugar/mediagallery/cell/MediaCellBindingAdapters.kt
+++ sher-gil/src/main/java/com/kinnerapriyap/sugar/mediagallery/cell/MediaCellBindingAdapters.kt
@@ -2,11 +2,16 @@ package com.kinnerapriyap.sugar.mediagallery.cell
 
 import android.content.ContentResolver
 import android.content.ContentUris
+import android.content.Context
 import android.content.res.Resources
+import android.graphics.drawable.Drawable
 import android.net.Uri
+import android.util.TypedValue
 import android.widget.ImageView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.google.android.material.card.MaterialCardView
 import com.kinnerapriyap.sugar.R
 import com.kinnerapriyap.sugar.mediagallery.MediaGalleryHandler.Companion.CAMERA_CAPTURE_ID
@@ -17,9 +22,31 @@ fun ImageView.bindMediaUri(mediaUri: Uri) {
         if (ContentUris.parseId(mediaUri) == CAMERA_CAPTURE_ID) {
             getCameraDrawableUri(resources)
         } else mediaUri
+    val errorDrawable =
+        getResourceFromAttr(R.attr.shergil_cardMediaErrorDrawable, context)
+    val placeholderDrawable =
+        getResourceFromAttr(R.attr.shergil_cardMediaPlaceholderDrawable, context)
+    val requestOptions =
+        RequestOptions().apply {
+            errorDrawable?.let { error(it) }
+            placeholderDrawable?.let { placeholder(it) }
+        }
     Glide.with(context)
         .load(uri)
+        .apply(requestOptions)
         .into(this@bindMediaUri)
+}
+
+private fun getResourceFromAttr(attribute: Int, context: Context): Drawable? {
+    var isValid: Boolean
+    val typedValue = TypedValue().apply {
+        isValid = context.theme.resolveAttribute(attribute, this, true)
+    }
+    return ContextCompat.getDrawable(
+        context,
+        if (isValid) typedValue.resourceId
+        else R.drawable.ic_placeholder_media
+    )
 }
 
 private fun getCameraDrawableUri(resources: Resources) =
