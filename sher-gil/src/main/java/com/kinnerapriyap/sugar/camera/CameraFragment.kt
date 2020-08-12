3035e362f010d8232723b132613e5859a393a859
kinnerapriyap <kinnerapriyap@gmail.com>
2020-08-12 19:18:23 +0900

    Expand wildcard import

--- sher-gil/src/main/java/com/kinnerapriyap/sugar/camera/CameraFragment.kt
+++ sher-gil/src/main/java/com/kinnerapriyap/sugar/camera/CameraFragment.kt
@@ -12,7 +12,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.Toast
-import androidx.camera.core.*
+import androidx.camera.core.Camera
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.ImageProxy
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
