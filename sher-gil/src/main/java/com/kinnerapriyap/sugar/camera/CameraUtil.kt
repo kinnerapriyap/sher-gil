6323a2e8e995cc55e76e2a597a909b6cf2d9a4d8
kinnerapriyap <kinnerapriyap@gmail.com>
2020-08-12 18:23:37 +0900

    Remove app name from attrs

--- sher-gil/src/main/java/com/kinnerapriyap/sugar/camera/CameraUtil.kt
+++ sher-gil/src/main/java/com/kinnerapriyap/sugar/camera/CameraUtil.kt
@@ -1,44 +1,19 @@
 package com.kinnerapriyap.sugar.camera
 
-import android.app.Activity
-import android.content.Context
-import android.util.TypedValue
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageProxy
 import androidx.camera.lifecycle.ProcessCameraProvider
-import com.kinnerapriyap.sugar.R
-import java.io.File
 import java.text.SimpleDateFormat
 import java.util.Locale
 
 const val ANIMATION_FAST_MILLIS = 50L
 const val ANIMATION_SLOW_MILLIS = 100L
 
-fun getOutputDirectory(activity: Activity): File {
-    val dirName = getStringResourceFromAttr(R.attr.shergil_appName, activity)
-    val mediaDir =
-        activity.externalMediaDirs.firstOrNull()?.let {
-            File(it, dirName).apply { mkdirs() }
-        }
-    return if (mediaDir != null && mediaDir.exists()) mediaDir else activity.filesDir
-}
-
-fun createFile(baseFolder: File, format: String, extension: String) =
-    File(
-        baseFolder,
-        SimpleDateFormat(format, Locale.US)
-            .format(System.currentTimeMillis()) + extension
-    )
-
-private fun getStringResourceFromAttr(attribute: Int, context: Context): String {
-    var isValid: Boolean
-    val typedValue = TypedValue().apply {
-        isValid = context.theme.resolveAttribute(attribute, this, true)
-    }
-    return context.resources.getString(
-        if (isValid) typedValue.resourceId
-        else R.string.shergil
-    )
-}
+fun getFileDisplayName(format: String): String =
+    SimpleDateFormat(format, Locale.ENGLISH)
+        .format(System.currentTimeMillis())
 
 /**
  * Determines whether or not the device has an available back camera
